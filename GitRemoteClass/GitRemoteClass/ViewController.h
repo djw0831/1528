15cdeaf1b4a2fd35dd3114721ba8e480b72d7e85
djw0831 <15620990351@163.com>
2015-10-23 15:17:08 +0800

    段骏威PLAY

---
+++ GitRemoteClass/GitRemoteClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitRemoteClass
+//
+//  Created by 段骏威 on 15/10/23.
+//  Copyright © 2015年 段骏威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+-(void)play;
+@end
+
---
+++ GitRemoteClass/GitRemoteClass/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  GitRemoteClass
+//
+//  Created by 段骏威 on 15/10/23.
+//  Copyright © 2015年 段骏威. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self play];
+}
+-(void)play{
+
+}
+
+@end
